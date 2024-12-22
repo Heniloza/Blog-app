1c65a5e4acfdf6bd90237eeee7f711652ffe5b62
Heniloza <ozahenil73@gmail.com>
2024-12-22 21:41:18 +0530

    database connection successfully done

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from 'express';
+import mongoose  from 'mongoose';
 
 const app = express();
 const PORT = 5000;
 
 //database connection
-const connectDb = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://ozahenil73:ozahenil73@cluster0.lxtlh.mongodb.net/blog/?retryWrites=true&w=majority&appName=Cluster0"
-    );
-    console.log("CONNECTED TO MONGODB");
-    
-  } catch (error) {
-    console.log("error in connecting to database",error);
-    
-  }
-};
+(function mongoDb(){
+  mongoose.connect("mongodb://127.0.0.1:27017/BlogAppMern")
+.then(()=> console.log("CONNECTED TO MOBGODB"))
+.catch((err)=> console.log("ERROR IN CONNECTING TO DATABASE",err)
+)})()
+
 
 app.listen(PORT, () => {
   console.log(`SERVER STARTED AT ${PORT} PORT`);
-  connectDb();
 });
